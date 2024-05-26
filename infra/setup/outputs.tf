cc31bdb2eaff50f3631af5b07d253f010f88b963
domgjntoki <fredsonmenezessumi@gmail.com>
2024-05-26 20:27:57 -0300

    Setup checks and test and linting

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,3 +8,13 @@ output "cd_user_access_key_secret" {
   value       = aws_iam_access_key.cd.secret
   sensitive   = true
 }
+
+output "ecr_repo_app" {
+  description = "ECR repository for the app image"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repo_proxy" {
+  description = "ECR repository for the proxy image"
+  value       = aws_ecr_repository.proxy.repository_url
+}
