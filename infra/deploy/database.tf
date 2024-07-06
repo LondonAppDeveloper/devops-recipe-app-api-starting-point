982b3fd61ff9fbeee7cf8f644c2218137dd9fdfe
purvender <purvender@gmail.com>
2024-07-06 20:35:03 +0530

    testjmknkewewss

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -36,9 +36,9 @@ resource "aws_db_instance" "main" {
   allocated_storage          = 20
   storage_type               = "gp2"
   engine                     = "postgres"
-  engine_version             = "15.4"
+  engine_version             = "16.1"
   auto_minor_version_upgrade = true
-  instance_class             = "db.t4g.micro"
+  instance_class             = "db.t3.micro"
   username                   = var.db_username
   password                   = var.db_password
   skip_final_snapshot        = true
