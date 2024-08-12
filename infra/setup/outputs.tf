4540134ab52de9295fafc2d38cc9d7269ce0b2d9
Dhanush Dinesh <dhanushndinesh@gmail.com>
2024-08-11 23:49:55 -0500

    Added ECR resources

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -9,4 +9,12 @@ output "cd_user_access_key_secret" {
   sensitive   = true
 }
 
+output "ecr_repo_app" {
+  value       = aws_ecr_repository.app.repository_url
+  description = "ECR repository URL for the app image"
+}
 
+output "ecr_repo_proxy" {
+  value       = aws_ecr_repository.proxy.repository_url
+  description = "ECR repository URL for the proxy image"
+}
