9d65d7d7a7c01b0db468e82343d341da8a46f01f
Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>
2024-10-07 21:21:46 +0000

    Added ecs task execution roles

---
+++ infra/deploy/templates/ecs/task-assume-role-policy.tf
@@ -0,0 +1,15 @@
+{
+	"Version": "2012-10-07",
+	"Statement" : [
+		{
+			"Sid": "",
+			"Effect": "Allow",
+			"Principal" {
+				"service": "ecs-task.amazonaws.com"
+			},
+			"Action": ["sts:AssumeRole"]
+		}
+
+	]
+}
+
