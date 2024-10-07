5cb5465ff9a97de50a0be8067c7ebae299bdfd88
Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>
2024-10-07 18:41:19 +0000

    added rds permission AddTagsToResource in policy

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -159,7 +159,8 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance"
+      "rds:ModifyDBInstance",
+      "rds:AddTagsToResource"
     ]
     resources = ["*"]
   }
