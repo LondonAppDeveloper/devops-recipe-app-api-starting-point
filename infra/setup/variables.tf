0575b7980149ef8add1f43ea98437a5ba777db60
elffirein <70919860+elffirein@users.noreply.github.com>
2024-10-06 21:57:55 -0400

    Readme update (#8)
    * updated readme
    
    * Corrected variable names in setup
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,11 +1,11 @@
 variable "tf_state_bucket" {
   description = "Name of S3 bucket in AWS for storing TF state"
-  default     = "devops-recipe-app-api-state"
+  default     = "gitops-recepie-app-tf-state"
 }
 
 variable "tf_state_lock_table" {
   description = "Name of DynamoDB table for TF state locking"
-  default     = "devops-recipe-app-api-tf-lock"
+  default     = "gitops-recepie-api-tf-lock"
 }
 
 variable "project" {
