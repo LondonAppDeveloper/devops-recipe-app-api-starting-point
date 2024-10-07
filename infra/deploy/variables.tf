0a33a060653365fde966d55833a868a838bd2cb8
Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>
2024-10-07 17:42:57 +0000

    added rds

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -12,3 +12,13 @@ variable "contact" {
   description = "Contact email for tagging resources"
   default     = "kaustuv.chatterjee@gmail.com"
 }
+
+variable "db_username" {
+	description = "Username for the recipe app api database"
+	default = "recipeapp"
+}
+
+variable "db_password" {
+	description = "Password for the Terraform database"
+}
+
