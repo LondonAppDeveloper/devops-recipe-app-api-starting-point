582f38221a04e3e7b096081b2e1e203ef5a71c37
Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>
2024-10-07 23:37:16 +0000

    Added security groups rules for ecs service to access RDS, end points

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -146,6 +146,42 @@ resource "aws_ecs_task_definition" "api" {
   }
 }
 
+resource "aws_security_group" "ecs_service" {
+  description = "Access rules for the ECS service."
+  name        = "${local.prefix}-ecs-service"
+  vpc_id      = aws_vpc.main.id
+
+
+  # Outbound access to endpoints
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  # RDS connectivity
+  egress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
+
+  # HTTP inbound access
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
