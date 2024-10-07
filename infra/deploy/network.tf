1cb428189282f2ee61a7e8254170899d36f14c1b
Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>
2024-10-07 04:34:07 +0000

    Created vpc

---
+++ infra/deploy/network.tf
@@ -0,0 +1,24 @@
+##########################
+# Network infrastructure #
+##########################
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+#########################################################
+# Internet Gateway needed for inbound access to the ALB #
+#########################################################
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-main"
+  }
+}
+
+
+
