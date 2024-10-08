55a8dd1d9670eef19591487cf1ed2189a09919f2
Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>
2024-10-08 23:06:20 +0000

    Added Load balancer support for ECS. Created target group for container service api, Created Listner to attach it tp load balancer & added forward rule to target group

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -30,3 +30,39 @@ resource "aws_security_group" "lb" {
   }
 }
 
+resource "aws_lb" "lb" {
+  name               = "${local.prefix}-lb"
+  load_balancer_type = "application"
+  subnets = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+  security_groups = [
+    aws_security_group.lb.id
+  ]
+}
+
+resource "aws_lb_target_group" "api" {
+  name        = "${local.prefix}-api"
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+  port        = 8000
+
+
+  health_check {
+    path = "/api/health-check/"
+  }
+}
+
+resource "aws_lb_listener" "api" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
+
