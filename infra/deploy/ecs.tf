f5abbbc164c7fd7b9b75c54545e2ec3236ac4e8e
Ubuntu <ubuntu@ip-172-31-46-137.ec2.internal>
2024-10-09 14:45:37 +0000

    ecs file formatted

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -201,9 +201,9 @@ resource "aws_security_group" "ecs_service" {
 
   # HTTP inbound access
   ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
+    from_port = 8000
+    to_port   = 8000
+    protocol  = "tcp"
     security_groups = [
       aws_security_group.lb.id
     ]
